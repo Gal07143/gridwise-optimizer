6db33acd15347c1abfde8d00612970e32902f4bc
Gal07143 <140276198+Gal07143@users.noreply.github.com>
2025-03-22 23:02:37 +0200

    Create MeterControls.tsx

---
+++ src/components/devices/controls/MeterControls.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ControlDialog from './ControlDialog';
+
+interface MeterControlsProps {
+  deviceId: string;
+}
+
+const MeterControls: React.FC<MeterControlsProps> = ({ deviceId }) => {
+  return (
+    <ControlDialog
+      title="Energy Meter"
+      description="View and reset meter counters"
+      actions={[
+        {
+          label: 'Reset Energy Counter',
+          onSubmit: () => console.log(`Reset energy counter on meter ${deviceId}`),
+        },
+        {
+          label: 'Sync Time',
+          onSubmit: () => console.log(`Sync time on meter ${deviceId}`),
+        }
+      ]}
+    />
+  );
+};
+
+export default MeterControls;
